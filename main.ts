2d691535b08642011ab225e7e7e1481ef9b6e1fa
indianscamme <144804933+indianscamme@users.noreply.github.com>
2024-05-28 13:51:59 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+let mySprite = sprites.create(img`
+    . . . . . . . f f f f f . . . . 
+    . . . . . . f e e e e e f . . . 
+    . . . . . f e e e d d d d f . . 
+    . . . . f f e e d f d d f d c . 
+    . . . f d d e e d f d d f d c . 
+    . . . c d b e e d d d d e e d c 
+    f f . c d b e e d d c d d d d c 
+    f e f . c f e e d d d c c c c c 
+    f e f . . f f e e d d d d d f . 
+    f e f . f e e e e f f f f f . . 
+    f e f f e e e e e e e f . . . . 
+    . f f e e e e f e f f e f . . . 
+    . . f e e e e f e f f e f . . . 
+    . . . f e f f b d f b d f . . . 
+    . . . f d b b d d c d d f . . . 
+    . . . f f f f f f f f f . . . . 
+    `, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 7 7 7 7 . . . . . . 
+    . . . . 7 7 f 2 2 2 7 7 . . . . 
+    . . . . 7 2 2 2 2 f 2 7 . . . . 
+    . . . 7 2 2 2 f 2 2 2 f 7 . . . 
+    . . . 7 f 2 2 2 2 2 2 2 7 . . . 
+    . . . 7 2 2 2 2 2 f 2 2 7 . . . 
+    . . . . 7 2 f 2 2 2 2 7 . . . . 
+    . . . . 7 7 2 2 f 2 7 7 . . . . 
+    . . . . . . 7 7 7 7 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
